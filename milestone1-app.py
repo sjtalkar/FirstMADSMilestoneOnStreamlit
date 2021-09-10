da879f814a111faf5c7ca767aa1a94e30bf212bb
sjtalkar <sjtalkar@live.com>
2021-09-10 12:25:51 -0700

    Checking if beta_columns works

--- milestone1-app.py
+++ milestone1-app.py
@@ -178,7 +178,7 @@ st.altair_chart(
 
 
 st.write(election_change_and_covid_death_df)
-# col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3, col4 = st.beta_columns(4)
 formatted_string = "{:.2f}".format(
     election_change_and_covid_death_df["deaths_avg_per_100k"].mean()
 )
